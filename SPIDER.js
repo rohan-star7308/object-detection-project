ee0e3055c96268d7de0306a57cb8f4b4debf4cee
rohan-star7308 <87434006+rohan-star7308@users.noreply.github.com>
2022-05-20 20:19:11 +0530

    Add files via upload

---
+++ SPIDER.js
@@ -0,0 +1,46 @@
+img="";
+status="";
+objects=[];
+
+function preload(){
+    img=loadImage("iron spider.jpg");
+}
+
+function setup(){
+    canvas=createCanvas(640,480);    
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelloaded);
+    document.getElementById("status").innerHTML="Status : Detecting objects";
+}
+
+function draw(){
+    image(img,0,0,640,480);
+    if(status != ""){
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status : Objects Detected";
+            fill("red");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
+
+function modelloaded(){
+    console.log("cocossd is initialized");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+
+}
